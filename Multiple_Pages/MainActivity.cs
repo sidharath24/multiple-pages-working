40849b4edc8fee5a75d1666e56c55c6a587877c1
visualstudio <visualstudio@122-PC30>
2019-04-11 11:50:49 -0400

    First

--- Multiple_Pages/MainActivity.cs
+++ Multiple_Pages/MainActivity.cs
@@ -5,24 +5,37 @@ using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
+using Android.Content;
 using Android.Widget;
+using Multiple_Pages.Resources.layout;
 
 namespace Multiple_Pages
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        public static EditText UserNameET, PasswordET;
+        Button LogInBtn;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            UserNameET = (EditText)FindViewById(Resource.Id.ETUserName);
+            PasswordET = (EditText)FindViewById(Resource.Id.ETPassword);
+            LogInBtn = (Button)FindViewById(Resource.Id.BtnLogin);
+            LogInBtn.Click += delegate
+            {
+                if (UserNameET.Text == "" || PasswordET.Text == "")
+                {
+                    Toast.MakeText(this, "Please enter username and password : ", ToastLength.Long).Show();
+                }
+                else if (UserNameET.Text == "admin" || PasswordET.Text == "12345678")
+                {
+                    Intent intent = new Intent(this, typeof(Page2Activity));
+                    StartActivity(intent);
+                }
+            };
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
---
+++ Multiple_Pages/Page2Activity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Multiple_Pages.Resources.layout
+{
+    [Activity(Label = "Page2Activity")]
+    public class Page2Activity : Activity
+    {
+        public static Button BackBtn;
+        public static TextView WelcomeTv;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.page2);
+            BackBtn = (Button)FindViewById(Resource.Id.BtnBack);
+            WelcomeTv = (TextView)FindViewById(Resource.Id.TvWelcome);
+
+            WelcomeTv.Text = "Hey " + MainActivity.UserNameET.Text + ". This is a new page";
+
+            BackBtn.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            };
+
+            // Create your application here
+        }
+    }
+}
